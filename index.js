8596dd595794d62e038c609eba22c344ca37667e
aronpadolina <47239184+aronpadolina@users.noreply.github.com>
2019-06-25 09:23:42 +0800

    Resolves #13

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ const admin = require('./node_modules/firebase-admin');
 const fs = require('fs');
 // Provides csv functions
 const fcsv = require('./node_modules/fast-csv');
-const csvtojson =require("./node_modules/csvtojson");
+const csvtojson = require("./node_modules/csvtojson");
 
 const serviceAccount = require("./service-key.json");
 //name of the collection
-let collectionKey = undefined; 
+let collectionKey = undefined;
 // Put CSV here, make sure the headers are appropriately labelled.
 let csv = undefined;
 
 // Replace credentials with our own, this one is a dummy sandbox I made
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-project-c19b4.firebaseio.com"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://fir-project-c19b4.firebaseio.com"
 });
 
 const firestore = admin.firestore();
-const settings = {timestampsInSnapshots: true};
+const settings = { timestampsInSnapshots: true };
 
 firestore.settings(settings);
 
 process.argv.forEach(function (val, index, array) {
-  if (index < 2) {
-  	return;
-  }
+	if (index < 2) {
+		return;
+	}
 
-  if (val.startsWith('csv=')) {
-  	let res = val.split('csv=');
-  	csv = res[1] + '.csv';
-  } 
-  else if (val === '--help') {
-  	console.log('Format: node index.js [collection] csv=[fileName]]');
-  } 
-  else if (collectionKey === undefined) {
-  	collectionKey = val;
-  }
+	if (val.startsWith('csv=')) {
+		let res = val.split('csv=');
+		csv = res[1] + '.csv';
+	}
+	else if (val === '--help') {
+		console.log('Format: node index.js [collection] csv=[fileName]]');
+	}
+	else if (collectionKey === undefined) {
+		collectionKey = val;
+	}
 
 });
 
@@ -46,11 +46,11 @@ if (collectionKey === undefined) {
 else if (csv === undefined) {
 	console.log('Argument \'csv\' is required');
 	console.log('Format: node index.js [collection] csv=[fileName]]');
-} 
+}
 else {
-	fs.createReadStream(csv).on('data', function(data) {
+	fs.createReadStream(csv).on('data', function (data) {
 		readCsv();
-	}).on('error', function(err) {
+	}).on('error', function (err) {
 		console.log('Error!');
 	});
 }
@@ -58,36 +58,40 @@ else {
 function readCsv() {
 	csvtojson().fromFile(csv).on('data', data => {
 		let stringData = data.toString('utf8');
-		if(collectionKey == 'quests') {
+		if (collectionKey == 'quests') {
 			stringData = generateKeywords(stringData);
 		}
 		upload(stringData);
 	})
-	.on('end', () => console.log('done'));
+		.on('end', () => console.log('done'));
 }
 
 function generateKeywords(stringData) {
 	let parsedData = JSON.parse(stringData);
-	if(parsedData.name) {
-		let keywords = parsedData.name.split(' ').map(k => k.toLowerCase());
-		parsedData.keywords = keywords;
+	if (parsedData.name) {
+		parsedData.keywords = getKeywords(parsedData.name);
 		return JSON.stringify(parsedData);
-	}else{
+	} else {
 		return stringData;
 	}
+}
+
+function getKeywords(value) {
+	return value.toLowerCase().replace(/[^\w ]/g, ' ').split(' ')
+		.filter(word => word ? word.length > 2 : false);
 }
 
 function upload(data) {
 	const parsedData = JSON.parse(data);
 	if (parsedData) {
 		firestore.collection(collectionKey)
-		.doc()
-		.set(parsedData)
-		.then((res) => {
-		    console.log("Document successfully written!");
-		}).catch((error) => {
-			console.error("Error writing document: ", error);
-		});
+			.doc()
+			.set(parsedData)
+			.then((res) => {
+				console.log("Document successfully written!");
+			}).catch((error) => {
+				console.error("Error writing document: ", error);
+			});
 	}
 }
 
@@ -97,13 +101,13 @@ function uploadWithDocKey(data) {
 	if (parsedData) {
 		Object.keys(parsedData).forEach(docKey => {
 			firestore.collection(collectionKey)
-			.doc(docKey)
-			.set(parsedData[docKey])
-			.then((res) => {
-		    	console.log("Document successfully written!");
-			}).catch((error) => {
-			   	console.error("Error writing document: ", error);
-			});
+				.doc(docKey)
+				.set(parsedData[docKey])
+				.then((res) => {
+					console.log("Document successfully written!");
+				}).catch((error) => {
+					console.error("Error writing document: ", error);
+				});
 		});
 	}
 }
